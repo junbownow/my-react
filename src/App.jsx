544d39c18d3b97812ab9db88e879a2750f0de2ed
junbownow <junbownow@gmail.com>
2025-11-05 18:37:57 +0900

    Reactプロジェクト初期設定

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Message from './Message';
+import UserInfo from './UserInfo';
+
+function App() {
+  return (
+    <div>
+      <Message text="こんにちは！Reactのpropsの練習中です！" />
+      <UserInfo name="純" age={29} />
+    </div>
+  );
+}
+
+export default App
---
+++ src/UserInfo.jsx
@@ -0,0 +1,11 @@
+function UserInfo({ name, age }) {
+  return (
+    <div>
+      <h2>ユーザー情報</h2>
+      <p>名前：{name}</p>
+      <p>年齢：{age}歳</p>
+    </div>
+  );
+}
+
+export default UserInfo;
