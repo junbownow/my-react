45f770d7f34a697c2fd9cb7447fc26de406f2dbd
junbownow <junbownow@gmail.com>
2025-11-12 12:20:16 +0900

    feat: Propsの基本コンポーネント作成とアロー関数の確認

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import './App.css';
-import UserCard from './components/UserCard';
+import "./App.css";
+import Greeting from './components/Greeting'; // 1. 子コンポーネントをインポート
 
-export default function App() {
-  const user = {
-    name: '純',
-    age: 32,
-    hobby: 'コーヒー',
-  };
+function App() {
+  // 2. Propsとして子コンポーネントに渡したいデータを用意します
+  const customMessage = "React学習頑張りましょう！";
+  const otherMessage = "これは2つ目のメッセージです。";
 
   return (
-    <div>
-      <h1>ユーザー情報</h1>
-      <UserCard user={user} />
-    </div>
+    <>
+      <h1>Propsの学習</h1>
+      
+      {/* 3. Greetingコンポーネントに Props (ここでは `message`) を渡します */}
+      {/* HTMLの属性のように記述します。**message="文字列"** */}
+      <Greeting message={customMessage} />
+      
+      {/* 4. 別の Props を渡して、再利用できることを確認します */}
+      <Greeting message={otherMessage} />
+
+      {/* 5. 文字列以外（例えば数値や変数）を渡す場合は、波括弧 {} で囲みます */}
+      <Greeting message="Propsで渡した情報がここに表示されます。" />
+    </>
   );
 }
+
+export default App;
