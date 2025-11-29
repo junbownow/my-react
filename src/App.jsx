9f2b76f81b98c10de70af7b8f04f27560262f36e
junbownow <junbownow@gmail.com>
2025-11-29 17:50:45 +0900

    chore: コールバック関数ドリルの最終コードを追加

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,36 @@ import './App.css';
 import NotifierButton from './components/Notifier/NotifierButton';
 
 const App = () => {
-  // 1.親の仕事：子から通知が来た時に実行する関数を定義
   const handleNotification = (message) => {
-    // messageには子から渡された文字列が入る
-    console.log('【通常受信】' + message);
+    console.log('親が受信：' + message);
   };
 
   const handleUrgentNotification = (message) => {
-    console.error("!!! 緊急アラート受信 !!!" + message);
+    console.error('!!! 緊急通知 !!!' + message);
+  };
+
+  const handleResetRequest = (source) => {
+    console.log(`[リセット]${source}から要求が来ました`);
+  };
+
+  const handleThankYou = () => {
+    console.log('親：いつもありがとう！');
+  };
+
+  const handleFailuer = (message) => {
+    console.log(`--- 失敗：${message} ---`);
   };
 
   return (
     <>
       <h1>コールバック関数ドリル</h1>
-
-      {/* 2.親の仕事：その関数をPropsとして子に渡す */}
       <NotifierButton buttonText="通知ボタンA" onNotify={handleNotification} />
       <NotifierButton buttonText="通知ボタンB" onNotify={handleNotification} />
-      <NotifierButton buttonText="緊急通知" onNotify={handleUrgentNotification} />
+      <NotifierButton buttonText="緊急ボタン" onNotify={handleUrgentNotification} />
+
+      <NotifierButton buttonText="リセット要求" onNotify={handleResetRequest} />
+      <NotifierButton buttonText="ありがとう" onNotify={handleThankYou} />
+      <NotifierButton buttonText="失敗通知" onNotify={handleFailuer} />
     </>
   );
 };
--- src/components/Notifier/NotifierButton.jsx
+++ src/components/Notifier/NotifierButton.jsx
@@ -2,8 +2,6 @@ import './NotifierButton.css';
 
 const NotifierButton = ({ buttonText, onNotify }) => {
   const handleClick = () => {
-    // 親から渡された関数 onNotify を呼び出す（実行する）
-    // 引数として、親がログに出したい「メッセージ」を渡す
     onNotify(buttonText + 'がクリックされました');
   };
 
