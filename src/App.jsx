46fa6cdf4f613699e079500ac14d18dd8387ff72
junbownow <junbownow@gmail.com>
2025-11-29 17:39:40 +0900

    feat: コールバック関数ドリルを完了し、複数の関数を子コンポーネントで実行可能に

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
 import './App.css';
-import Counter from './components/Counter/Counter';
+import NotifierButton from './components/Notifier/NotifierButton';
 
 const App = () => {
-  // 1. 親側で「実行したい関数」を定義する
-  const handleShowMessage = (teamName) => {
-    console.log(`${teamName}のリセットボタンが押されました`);
-    alert(`${teamName}がリセットされました！`);
+  // 1.親の仕事：子から通知が来た時に実行する関数を定義
+  const handleNotification = (message) => {
+    // messageには子から渡された文字列が入る
+    console.log('【通常受信】' + message);
+  };
+
+  const handleUrgentNotification = (message) => {
+    console.error("!!! 緊急アラート受信 !!!" + message);
   };
 
   return (
     <>
-      <h1>カウントアップ大会</h1>
-      <div className="app-container">
-        {/* 
-        2.定義した関数をPropsとして渡す
-        習慣として渡すProps名は"on　+　動作"（onReset）にすることが多いです
-        */}
-        <Counter title="Aチーム" color="red" startValue={10} onReset={handleShowMessage} />
-        <Counter title="Bチーム" color="blue" startValue={100} onReset={handleShowMessage} />
-      </div>
+      <h1>コールバック関数ドリル</h1>
+
+      {/* 2.親の仕事：その関数をPropsとして子に渡す */}
+      <NotifierButton buttonText="通知ボタンA" onNotify={handleNotification} />
+      <NotifierButton buttonText="通知ボタンB" onNotify={handleNotification} />
+      <NotifierButton buttonText="緊急通知" onNotify={handleUrgentNotification} />
     </>
   );
 };
